07c8e547c14d6e395d7ba45c375d8096e406429d
Bogdan Cuza <bogdancuza@protonmail.com>
2022-06-28 23:09:57 +0300

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,13 @@
 class DogsController < ApplicationController
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def index
+        if params[:sorted].nil?
+            @dogs = Dog.all
+        else
+            @dogs = Dog.all.sort_by(&:employee_count).reverse!
+        end
+    end
 end
